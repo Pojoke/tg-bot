93c2afa1022c23d65a022c1830721813fa3510b7
Pojoke <pavlykdmutro@gmail.com>
2024-12-07 13:30:49 +0200

    tg_bot

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>TG</h1>
+    <form action="telegram.php" method="post">
+        <input type="text" name="message">
+        <input type="submit" name="submit">
+</form>
+</body>
+</html>
---
+++ token.php
@@ -0,0 +1,4 @@
+
+<?php
+const TOKEN='7881468618:AAEHa84HzbSx93eCqRQqSrn2o3MrI_gyfkE';
+const CHANNEL=''
